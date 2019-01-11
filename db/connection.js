e85bef480217c2d46a2fcb1e3a1012d46f905aa3
Karelys Souto <karelyssouto@icloud.com>
2019-01-11 12:47:03 -0500

    push to heroku please and thank you

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose")
+require('dotenv').config()
 
-mongoose.connect("mongodb://localhost/your-data-base-name").then(() => {
+mongoose.connect(process.env.MONGODB_URI).then(() => {
     console.log("MONGODB is now connected")
 })
 
